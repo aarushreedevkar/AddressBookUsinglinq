e5e3a5fec4914f99110648c1d359a82b493e3c2c
jyoti <jyotidhage10@gmail.com>
2022-09-15 16:35:47 +0530

    [Aaradhya]Add:Create a Contact in AddressBook

---
+++ AddressBookDataTable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookUsingLinq
+{
+    public  class AddressBookDataTable
+    {
+        //Creating DataTable for addressbook problem UC1
+        DataTable dataTable = new DataTable();
+
+        /// Create the Address Book table and add attributes.
+
+        public void createAddressBookTable()
+        {
+            dataTable.Columns.Add("FirstName", typeof(string));
+            dataTable.Columns.Add("LastName", typeof(string));
+            dataTable.Columns.Add("Address", typeof(string));
+            dataTable.Columns.Add("City", typeof(string));
+            dataTable.Columns.Add("State", typeof(string));
+            dataTable.Columns.Add("ZipCode", typeof(int));
+            dataTable.Columns.Add("PhoneNumber", typeof(long));
+            dataTable.Columns.Add("Email", typeof(string));
+
+        }
+
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AddressBookUsingLinq
+{
+    class program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to Address Book using Linq");
+        }
+    }
+}
